66676fc774ac65b2220ef9343d66959254449f98
IliaValkov <ilia.valkov97@gmail.com>
2019-05-18 14:41:55 +0200

    added image and experimenting with width and height

--- frontend/screens/LocationPlanScreen.js
+++ frontend/screens/LocationPlanScreen.js
@@ -1,12 +1,13 @@
 import React, {Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button , Image , Dimensions} from "react-native";
 
 
 class LocationPlanScreen extends Component {
+
   render(){
     return(
-      <View style = { styles.container } >
-          <Text>Location Plan for Windows</Text>
+      <View style = { styles.container } > 
+          <Image  source={require('../assets/divaeBuroPlan.png')} style={styles.image} />
       </View>
     );
   }
@@ -18,6 +19,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#565656',
+  }, 
+  image: {
+    width: 300, 
+    height: 400,
   }
 });
