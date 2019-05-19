0716310b042be9ae4e9728fb620123a6b3881a8f
IliaValkov <ilia.valkov97@gmail.com>
2019-05-19 16:33:01 +0200

    adjusted picture size and prototyped window indicator rendering

--- frontend/screens/LocationPlanScreen.js
+++ frontend/screens/LocationPlanScreen.js
@@ -1,13 +1,35 @@
 import React, {Component } from "react";
 import { View, Text, StyleSheet, Button , Image , Dimensions} from "react-native";
 
+const windowList = [
+  {
+    id: '1',
+    x: 25 , 
+    y: 160 , 
+    isOpen: true,
+  },
+  {
+    id: '2',
+    x: 100 , 
+    y: 160 , 
+    isOpen: true,
+  },
+  {
+    id: '3',
+    x: 60 , 
+    y: 160 , 
+    isOpen: false,
+  },
+]
 
 class LocationPlanScreen extends Component {
 
   render(){
+    let windows = windowList.map( w => <View key={w.id} style={styles.dot} left= {w.x} top = {w.y} backgroundColor = { w.isOpen ? '#00FF7F' : '#FF3333' }></View> ) 
     return(
       <View style = { styles.container } > 
-          <Image  source={require('../assets/divaeBuroPlan.png')} style={styles.image} />
+          <Image source={require('../assets/divaeBuroPlan.png')} style={styles.image} resizeMode = 'stretch'/>
+          {windows}
       </View>
     );
   }
@@ -20,10 +42,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#565656',
+    borderWidth: Dimensions.get('window').width * 0.05,
+    borderStyle: 'solid', 
+    borderColor: '#999999', 
+    position: 'relative', 
   }, 
   image: {
-    width: 300, 
-    height: 400,
+    width: '100%', 
+    height: '100%',
+  },
+  dot: {
+    position:'absolute',
+    width: '3%',
+    height: '1%',
+    borderRadius: 5,
+    flex: 1,
   }
 });
