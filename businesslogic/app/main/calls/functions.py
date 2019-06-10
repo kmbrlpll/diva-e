a73546427475485d798be6cd5428889e03b3cdec
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2019-06-10 14:40:27 +0200

    renaming

--- businesslogic/app/main/calls/functions.py
+++ businesslogic/app/main/calls/functions.py
@@ -7,23 +7,6 @@ from businesslogic.app.main.calls.get_channels import get_channel_state, get_cha
 
 routing = Blueprint('routing', __name__)
 
-@routing.route('/floorplanurl', methods=['GET'])
-def get_floorplan_url():
-    try:
-        with open('C:\\Users\\ilona\\Desktop\\SS19\\diva-e-praxisprojekt\\businesslogic\\app\\test\\src\\test_office_config.json') as json_file:
-            data = json.load(json_file)
-            floorplan_info = {
-                "image_url" : data["path"],
-                "office_name" : data["name"]
-            }
-    except:
-        floorplan_info = {
-            "error_code" : 404,
-            "message" : "Floorplan data could not be loaded from configuration."
-
-        }
-    return str(floorplan_info)
-
 
 @routing.route('/')
 def index():
