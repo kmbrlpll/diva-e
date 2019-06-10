2949aa17e4063f01bfe3083193b832ffb49e64cd
Caroline <clnienkerke@gmail.com>
2019-06-10 14:44:16 +0200

    renamed more stuff

--- businesslogic/app/main/calls/functions.py
+++ businesslogic/app/main/calls/functions.py
@@ -1,50 +1,39 @@
-from flask import Flask
-from flask import request
-from flask import send_file
-from flask import Blueprint
+import requests
 import json
-from businesslogic.app.main.calls.get_channels import get_channel_state, get_channels
 
-routing = Blueprint('routing', __name__)
+# api calls to get states of a single channel (url_state) and a list with all the thing configs (url_things)
+url_state = 'http://diva-e-iot-lab.northeurope.cloudapp.azure.com:8080/api/states/things/'
+url_things = 'http://diva-e-iot-lab.northeurope.cloudapp.azure.com:8080/api/configuration/'
+headers = {
+    'content-type': 'application/json',
+    'x-api-key': '44W8wJoAgaMMyeVxwo7GDanwtsMZbXXB'
+}
+
+# requests api and returns the state of a single channel
+def get_channel_state(channel_id, thing_id):
+    r_state = requests.get(url_state + thing_id + "/channels/" + channel_id, {}, headers=headers)
+    state = r_state.json()["state"]["reported"]
+    return state
 
 
-@routing.route('/')
-def index():
-        return 'Hi! You are at the root directory of the API.\n The API paths begin from /api/'
+# takes channel type (window/heater) as parameter, makes api call and
+# returns a dictionary with channel info
+def get_channels(c_type):
+    all_channels = {}
+    r_things = requests.get(url_things, {}, headers=headers)
+    with open('C:\\Users\\ilona\\Desktop\\SS19\\diva-e-praxisprojekt\\businesslogic\\app\\test\\src\\CarosStructure.json') as json_file:
+            data = json.load(json_file)
 
-
-# returns a json of all channels that represent an open window
-@routing.route('/openwindows', methods=['GET'])
-def get_open_windows():
-    response = {}
-
-    open_windows = get_channels("window")
-
-    for k,v in open_windows.items():
-        channel_state = get_channel_state(k,v["thing_id"])
-        v["state"] = channel_state
-        # throwing thing id and type out of the json because not relevant for us
-        del open_windows[k]["thing_id"]
-        del open_windows[k]["type"]
-        # currently the thing channels i setup in swagger have no states yet so this returns empty
-        # comment out if you wanna see some results ;)
-        if channel_state != "open":
-           del open_windows[k]
-           # TODO: error handling in case state=null
-
-    return json.dumps(open_windows)
-
-@routing.route('/runningheaters', methods=['GET'])
-def get_running_heaters():
-    open_windows = get_channels("heater")
-
-    for k,v in open_windows.items():
-        channel_state = get_channel_state(k,v["thing_id"])
-        v["state"] = channel_state
-        del open_windows[k]["thing_id"]
-        del open_windows[k]["type"]
-        #if float(channel_state) <= threshold:
-           #del open_windows[k]
-           # TODO: error handling in case state=null
-    return json.dumps(open_windows)
-
+    for i in data["things"]:
+        for thing in  i["channels"]:
+            channel_type = thing["properties"]["type"]
+            if channel_type == c_type:
+                channel = {}
+                channel["x"] = int(thing["properties"]["x"])
+                channel["y"] = int(thing["properties"]["y"])
+                channel["type"] = channel_type
+                channel["thing_id"] = i["id"]
+                all_channels[thing["id"]] = channel
+            else:
+                continue
+    return all_channels
