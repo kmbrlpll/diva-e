95b52175e06aca5e5758d6e84ce8bcad7b26c247
Tim Hirselandt <tim@Tims-MBP.fritz.box>
2019-06-11 20:34:17 +0200

    Error, Loader, InfoContainer components

---
+++ frontend/components/Error.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default class Error extends React.Component {
+
+    renderMessage = () => {
+		if(this.props.error_code == null){
+			return(
+				<Text style={styles.code}>ERROR</Text>
+                <Text style={styles.message}>An error happened, please try again!</Text>
+			);
+		} else if(this.props.error_code == undefined) {
+			return (
+				<Text style={styles.code}>ERROR</Text>
+                <Text style={styles.message}>An error happened, please try again!</Text>
+			);
+		} else {
+			return(
+				<Text style={styles.code}>{this.props.error_code}</Text>
+                <Text style={styles.message}>{this.props.message}</Text>
+			);
+		}
+    }
+	
+	render() {
+        return (
+            <View style={styles.container}>
+                  {this.renderMessage()}
+            </View>
+       );
+    }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+  },
+  code: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingBottom: 20,
+  },
+  message: {
+    fontSize: 14,
+    textAlign: 'center',
+    paddingTop: 20,
+  }
+});
