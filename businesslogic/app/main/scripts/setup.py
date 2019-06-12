df25efd4b6626b767b25b90d29479f34f7bf6046
Caroline <clnienkerke@gmail.com>
2019-06-12 14:33:15 +0200

    added functs.py as functions that need to be overwritten in functions.py and routes.py to be pasted into api_calls instead of the current routes there. Thanks Ilona for taking care of that! <3

--- businesslogic/app/main/scripts/setup.py
+++ businesslogic/app/main/scripts/setup.py
@@ -2,55 +2,55 @@ import requests
 import json
 from datetime import datetime
 
-url = "http://diva-e-iot-lab.northeurope.cloudapp.azure.com:8080/api/configuration/things/"
+url = "http://diva-e-iot-lab.northeurope.cloudapp.azure.com:8080/api/"
 headers = {
 'content-type': 'application/json',
 'x-api-key': '44W8wJoAgaMMyeVxwo7GDanwtsMZbXXB'
 }
 
-def save_tokens_to_file(tokens):
-    now = datetime.now().strftime('%Y_%m_%d')
-    try:
-        with open('../../test/src/setup_things_' + now + '.json', 'w') as outfile:
-            json.dump(tokens, outfile)
-        print("saved JSON!")
-    except:
-        print("could not save tokens to file!")
 
+def get_thing(id):
+    thing = requests.get(url + "configuration/things/" + id, {}, headers=headers).json()
+    return thing
 
 def main():
     tokens = []
     #error handling here!
-    try:
-        with open('../../test/src/test_office_config.json') as json_file:
-            data = json.load(json_file)
-            all_things = data["things"]
 
-        for thing in all_things:
-            #error handling here!
-            thing_id = thing.get("id")
-            thing_data = thing.get("put")
-            token = {"id" :thing_id }
+    with open('../../test/src/test_office_config.json') as json_file:
+        data = json.load(json_file)
+        all_things_from_config = data["things"]
 
-            print("putting thing " + thing_id + "...")
-            try:
-                put_request = requests.put(url + thing_id, data=json.dumps(thing_data), headers=headers)
-                print(put_request.status_code)
+    print("*****")
+    for configured_thing in all_things_from_config:
 
-                #WHAT THE FUCK ARE WE DOING WITH THESE TOKENS?????
-                #Im currently just storing them in a file but Sven advised us to specifically NOT do that
-                token["token"] = put_request.json()["token"]
-                tokens.append(token)
+        configured_thing_id = configured_thing.get("id")
+        #retrieve the thing object via api call to make alterations to channel properties
+        installed_thing = get_thing(configured_thing_id)
 
-                if put_request.status_code == 200:
-                    print("Sucessfully put " + thing_id + "!")
 
-                save_tokens_to_file(tokens)
-            except:
-                print("Thing " + thing_id + " could not be uploaded to the server.")
+        for installed_channel in installed_thing["channels"]:
+            installed_channel_id = installed_channel.get("id")
+            configured_properties = configured_thing["channels"][installed_channel_id]
 
-    except:
-        print("Loading config file failed!")
+            installed_channel["properties"] = configured_properties
+
+
+            print("putting properties in channel " + installed_channel_id + "...")
+        try:
+            updated_thing_id = installed_thing["id"]
+            del installed_thing["id"]
+            put_request = requests.put(url + "configuration/things/" + updated_thing_id, data=json.dumps(installed_thing), headers=headers)
+
+            if put_request.status_code < 300:
+                print("Sucessfully configured thing " + updated_thing_id + "!")
+                print("*****")
+            else:
+                print("Thing " + updated_thing_id + "could not be updated!")
+                print("*****")
+
+        except:
+            print("Thing " + thing_id + " could not be uploaded to the server.")
 
 
 
